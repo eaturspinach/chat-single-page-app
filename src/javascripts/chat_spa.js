b22fda8de40b8269f7e34bf27f4d50dfbc8e7e28
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2012-10-30 15:43:23 -0700

    ok, js architecture is successfully updating DOM now

--- src/javascripts/chat_spa.js
+++ src/javascripts/chat_spa.js
@@ -9,57 +9,10 @@
 /* global $, ChatSPA */
 
 var ChatSPA = (function($) {
-
-	var
-		configMap = {
-			extended_height: 434,
-			extended_title: 'Click to retract',
-			retracted_height: 16,
-			retractekd_title: 'Click to extend',
-			template_html: '<div class="chat-spa-slider"></div>'
-		},
-		$chatSlider,
-		toggleSlider, onClickSlider, initModule;
-		
-	// DOM method
-	toggleSlider = function(){
-		var 
-			slider_height = $chatSlider.height();
-	
-		// extend slider if fully retracted
-		if(slider_height === configMap.retracted_height){
-			$chatSlider
-				.animate({height:configMap.extended_height})
-				.attr('title', configMap.extended_title);
-			return true;
-		} 
-		// retract slider if fully extended
-		else if (slider_height === configMap.extended_height){
-			$chatSlider
-				.animate({height: configMap.retracted_height})
-				.attr('title', configMap.retracted_title);
-			return true;
-		}
-		// do not take action if slider is in transition
-		return false;
-	};
-	
-	// Event handler
-	onClickSlider = function (event) {
-		toggleSlider();
-		return false;
-	}
 	
 	// Public method
 	initModule = function ($container) {
-		ChatSPA.shell.initModule($('#chat-spa'));
-		$container.html(configMap.template_html);
-		$chatSlider = $container.find('.chat-spa-slider');
-		
-		$chatSlider
-			.attr('title',configMap.retracted_title)
-			.click(onClickSlider);
-		return true;
+		ChatSPA.shell.initModule($container);
 	}
 	
 	return {initModule : initModule};
