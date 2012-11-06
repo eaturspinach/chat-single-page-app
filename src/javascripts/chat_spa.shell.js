32e816c7bcb3c0991227fe459d2805deae0f8593
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2012-11-05 17:13:16 -0800

    more code

--- src/javascripts/chat_spa.shell.js
+++ src/javascripts/chat_spa.shell.js
@@ -8,9 +8,13 @@
 /*global $, ChatSPA */
 
 ChatSPA.shell = (function (){
-
-  var
-    configMap = {
+  
+	//----------------- BEGIN MODULE SCOPE VARIABLES ---------------
+	var
+	  	configMap = {
+		anchor_schema_map : {
+			chat : { open:true, close:true }
+		},
 		main_html :
 			'<div class="chat-spa-shell-head">'
 			+ '<div class="chat-spa-shell-head-logo"></div>'
@@ -28,13 +32,25 @@ ChatSPA.shell = (function (){
 			chat_retract_time	: 300,
 			chat_extend_height	: 450,
 			chat_retract_height	: 15
-	},
-	stateMap  = {},
-	jqueryMap = {},
-	setJqueryMap, initModule;
+		},
+		stateMap  = {
+		anchor_map		 	: {},
+		is_chat_retracted	: true
+		},
+		jqueryMap = {},
+		copyAnchorMap, setJqueryMap, toggleChat,
+		changeAnchorPort, onChangeAddress, 
+		onClickChat, initModule;
+	//----------------- END MODULE SCOPE VARIABLES ---------------
+	
+	//------------------- BEGIN UTILITY METHODS ------------------
+	// Returns copy of stored anchor map; minimizes overhead
+	copyAnchorMap = function () {
+		return $.extend( true, {}, stateMap.anchor_map );
+	};
+	//-------------------- END UTILITY METHODS -------------------
 	
 	// ------------------- Begin DOM Method ------------------- 
-	
 	// Begin DOM method /setJqueryMap/
 	setJqueryMap = function (){
 		var $container = stateMap.$container;
@@ -87,6 +103,51 @@ ChatSPA.shell = (function (){
 		// End retract Chat Slider
 	};
 	// End toggleChat
+	
+	// Begin DOM method /changeAnchorPart/ - Changes part of the URI anchor component
+	changeAnchorPort = function(arg_map){
+		var
+			anchor_map_revise = copyAnchorMap(),
+			bool_return = true,
+			key_name, key_name_dep;
+			
+		// Begin merge changes into anchor map
+		KEYVAL:
+		for (key_name in arg_map){
+			if(arg_map.hasOwnProperty(key_name)){
+				
+				// skip dependent keys during iteration
+				if(key_name.indexOf('_') === 0 ) { continue KEYVAL; }
+				
+				// update independent key value
+				anchor_map_revise[key_name] = arg_map[key_name];
+				
+				// update matching dependent key
+				key_name_dep = '_' + key_name;
+				if(arg_map[key_name_dep]){
+					anchor_map_revise[key_name_dep] = arg_map[key_name_dep];
+				} else {
+					delete anchor_map_revise[key_name_dep];
+					delete anchor_map_revise['_s' + key_name_dep];
+				}
+				
+			}
+		}
+		// End merge changes into anchor map
+		
+		// Begin attempt to update URI; revert if not successful
+		try {
+			$.uriAnchor.setAnchor( anchor_map_revise );
+		} catch ( error ) {
+			// replace URI with existing state
+			$.uriAnchor.setAnchor( stateMap.anchor_map,null,true );
+			bool_return = false;
+		}
+			// End attempt to update URI...
+		  	return bool_return;
+	};
+	// End DOM method /changeAnchorPart/
+	
 	// ------------------- End DOM Method ------------------- 
 	
 	
